222e5c0e09d36ede4c635ba328a9c476096010b0
Maximilian Philipps <mphilipps@saltation.de>
2016-04-18 13:16:37 +0200

    some changes to make lintian happy

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mod-log-ipmask):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="mod-log-ipmask" command="/usr/bin/mod-log-ipmask"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libmod-log-ipmask 1.0-1 mod-log-ipmask (>> 1.0-1-0), mod-log-ipmask (<< 1.0-1-99)
